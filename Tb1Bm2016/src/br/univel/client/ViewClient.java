5c94fb32a6cc3c63a93a664849ab217bfa0f6546
Emerson Magalhaes <emdemagalhaes@gmail.com>
2016-04-22 17:49:58 -0300

    Cliado os Jframe ViewClient e ViewServer

--- Tb1Bm2016/src/br/univel/client/ViewClient.java
+++ Tb1Bm2016/src/br/univel/client/ViewClient.java
@@ -2,14 +2,41 @@ package br.univel.client;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.rmi.RemoteException;
+import java.rmi.registry.Registry;
+import java.util.List;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 
-public class ViewClient extends JFrame {
+import br.univel.common.Client;
+import br.univel.common.Server;
+import javax.swing.JTabbedPane;
+import javax.swing.JSplitPane;
+import javax.swing.JScrollPane;
+import javax.swing.JList;
+import javax.swing.JTable;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class ViewClient extends JFrame implements Client {
 
 	private JPanel contentPane;
+	private JTextField txfIp;
+	private JTextField txfPorta;
+	private JTextField txfMensagem;
+	private JButton buttonDesconectar;
+	private JButton buttonConectar;
+	private JLabel lblNome;
+	private JTextField txfMeuNome;
+	private JButton buttonEnviar;
 
 	/**
 	 * Launch the application.
@@ -20,6 +47,12 @@ public class ViewClient extends JFrame {
 				try {
 					ViewClient frame = new ViewClient();
 					frame.setVisible(true);
+
+					// =======================================================================================
+					frame.configurar(); // Só essa chamada não foi feita pelo
+										// Window Builder.
+					// =======================================================================================
+
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -27,16 +60,200 @@ public class ViewClient extends JFrame {
 		});
 	}
 
+
 	/**
 	 * Create the frame.
 	 */
 	public ViewClient() {
+		setTitle("Torrent RMI");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 745, 468);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+
+
+
+		JPanel panel_1 = new JPanel();
+		contentPane.add(panel_1, BorderLayout.NORTH);
+		GridBagLayout gbl_panel_1 = new GridBagLayout();
+		gbl_panel_1.columnWidths = new int[] { 0, 0, 0, 0, 0, 0, 0 };
+		gbl_panel_1.rowHeights = new int[] { 0, 0, 0 };
+		gbl_panel_1.columnWeights = new double[] { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE };
+		gbl_panel_1.rowWeights = new double[] { 0.0, 0.0, Double.MIN_VALUE };
+		panel_1.setLayout(gbl_panel_1);
+
+		lblNome = new JLabel("Nome");
+		GridBagConstraints gbc_lblNome = new GridBagConstraints();
+		gbc_lblNome.anchor = GridBagConstraints.EAST;
+		gbc_lblNome.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNome.gridx = 0;
+		gbc_lblNome.gridy = 0;
+		panel_1.add(lblNome, gbc_lblNome);
+
+		txfMeuNome = new JTextField();
+		GridBagConstraints gbc_txfMeuNome = new GridBagConstraints();
+		gbc_txfMeuNome.gridwidth = 3;
+		gbc_txfMeuNome.insets = new Insets(0, 0, 5, 5);
+		gbc_txfMeuNome.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txfMeuNome.gridx = 1;
+		gbc_txfMeuNome.gridy = 0;
+		panel_1.add(txfMeuNome, gbc_txfMeuNome);
+		txfMeuNome.setColumns(10);
+
+		JLabel lblNewLabel = new JLabel("Endereço IP");
+		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
+		gbc_lblNewLabel.insets = new Insets(0, 0, 0, 5);
+		gbc_lblNewLabel.anchor = GridBagConstraints.EAST;
+		gbc_lblNewLabel.gridx = 0;
+		gbc_lblNewLabel.gridy = 1;
+		panel_1.add(lblNewLabel, gbc_lblNewLabel);
+
+		txfIp = new JTextField();
+		txfIp.setText("127.0.0.1");
+		GridBagConstraints gbc_txfIp = new GridBagConstraints();
+		gbc_txfIp.insets = new Insets(0, 0, 0, 5);
+		gbc_txfIp.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txfIp.gridx = 1;
+		gbc_txfIp.gridy = 1;
+		panel_1.add(txfIp, gbc_txfIp);
+		txfIp.setColumns(10);
+
+		JLabel lblNewLabel_1 = new JLabel("Porta");
+		GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
+		gbc_lblNewLabel_1.anchor = GridBagConstraints.EAST;
+		gbc_lblNewLabel_1.insets = new Insets(0, 0, 0, 5);
+		gbc_lblNewLabel_1.gridx = 2;
+		gbc_lblNewLabel_1.gridy = 1;
+		panel_1.add(lblNewLabel_1, gbc_lblNewLabel_1);
+
+		txfPorta = new JTextField();
+		txfPorta.setText("1818");
+		GridBagConstraints gbc_txfPorta = new GridBagConstraints();
+		gbc_txfPorta.insets = new Insets(0, 0, 0, 5);
+		gbc_txfPorta.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txfPorta.gridx = 3;
+		gbc_txfPorta.gridy = 1;
+		panel_1.add(txfPorta, gbc_txfPorta);
+		txfPorta.setColumns(10);
+
+		buttonConectar = new JButton("Conectar");
+		GridBagConstraints gbc_buttonConectar = new GridBagConstraints();
+		gbc_buttonConectar.insets = new Insets(0, 0, 0, 5);
+		gbc_buttonConectar.gridx = 4;
+		gbc_buttonConectar.gridy = 1;
+		panel_1.add(buttonConectar, gbc_buttonConectar);
+
+		buttonDesconectar = new JButton("Desconectar");
+		GridBagConstraints gbc_buttonDesconectar = new GridBagConstraints();
+		gbc_buttonDesconectar.gridx = 5;
+		gbc_buttonDesconectar.gridy = 1;
+		panel_1.add(buttonDesconectar, gbc_buttonDesconectar);
+		
+		tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		contentPane.add(tabbedPane, BorderLayout.CENTER);
+		
+		panel_2 = new JPanel();
+		tabbedPane.addTab("Arquivos", null, panel_2, null);
+		panel_2.setLayout(new BorderLayout(0, 0));
+		
+		panel_3 = new JPanel();
+		panel_2.add(panel_3, BorderLayout.SOUTH);
+		
+		btnDownlod = new JButton("Downlod");
+		panel_3.add(btnDownlod);
+		
+		
+		splitPane = new JSplitPane();
+		panel_2.add(splitPane, BorderLayout.CENTER);
+		
+		scrollPane = new JScrollPane();
+		splitPane.setLeftComponent(scrollPane);
+		
+		list = new JList();
+		scrollPane.setViewportView(list);
+		
+		scrollPane_1 = new JScrollPane();
+		splitPane.setRightComponent(scrollPane_1);
+		
+		list_1 = new JList();
+		scrollPane_1.setViewportView(list_1);
+		
+		panel = new JPanel();
+		tabbedPane.addTab("Downloads", null, panel, null);
+		panel.setLayout(new BorderLayout(0, 0));
+		
+		panel_4 = new JPanel();
+		panel.add(panel_4, BorderLayout.SOUTH);
+		
+		btnNewButton_2 = new JButton("Iniciar");
+		btnNewButton_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		panel_4.add(btnNewButton_2);
+		
+		btnNewButton_1 = new JButton("Pausar");
+		panel_4.add(btnNewButton_1);
+		
+		btnNewButton = new JButton("Cancelar");
+		panel_4.add(btnNewButton);
+		
+		table = new JTable();
+		panel.add(table, BorderLayout.CENTER);
+		
+		
 	}
+	protected void configurar() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	private Server server;
+
+	/**
+	 * Referência a esse próprio objeto depois de exportado, passado para o
+	 * servidor.
+	 */
+	private Client client;
+
+	/**
+	 * Registo onde o objeto exportado será buscado pelo nome. É o registro que
+	 * escuta na porta TCP/IP, aberto no servidor.
+	 */
+	private Registry registry;
+	private JTabbedPane tabbedPane;
+	private JPanel panel;
+	private JPanel panel_2;
+	private JPanel panel_3;
+	private JSplitPane splitPane;
+	private JScrollPane scrollPane;
+	private JList list;
+	private JScrollPane scrollPane_1;
+	private JList list_1;
+	private JButton btnDownlod;
+	private JPanel panel_4;
+	private JButton btnNewButton;
+	private JButton btnNewButton_1;
+	private JButton btnNewButton_2;
+	private JTable table;
+
+	@Override
+	public void listaClients(List<String> lista) throws RemoteException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void listaArquivos(String remetente, String mensagem) throws RemoteException {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+
 
 }
--- Tb1Bm2016/src/br/univel/server/ViewServer.java
+++ Tb1Bm2016/src/br/univel/server/ViewServer.java
@@ -1,15 +1,63 @@
 package br.univel.server;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Vector;
+import java.util.Map.Entry;
 
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 
-public class ViewServer extends JFrame {
+import br.univel.common.Client;
+import br.univel.common.Server;
+
+public class ViewServer extends JFrame implements Server{
+
+
+	// =======================================================================================
+	//
+	// CÃ³digo gerado pelo Window Builder
+	//
+	// =======================================================================================
 
 	private JPanel contentPane;
+	private JTextField txfPorta;
+	private JButton buttonIniciarServico;
+	private JButton buttonPararServico;
+	private JComboBox comboIp;
+	private JTextArea textArea;
 
 	/**
 	 * Launch the application.
@@ -20,6 +68,12 @@ public class ViewServer extends JFrame {
 				try {
 					ViewServer frame = new ViewServer();
 					frame.setVisible(true);
+
+					// =======================================================================================
+					frame.configurar(); // SÃ³ essa chamada nÃ£o foi feita pelo
+										// Window Builder.
+					// =======================================================================================
+
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -31,12 +85,317 @@ public class ViewServer extends JFrame {
 	 * Create the frame.
 	 */
 	public ViewServer() {
+		setTitle("RMI Chat Server");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 574, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		GridBagLayout gbl_contentPane = new GridBagLayout();
+		gbl_contentPane.columnWidths = new int[] { 0, 0, 0, 0, 0 };
+		gbl_contentPane.rowHeights = new int[] { 0, 0, 0, 0 };
+		gbl_contentPane.columnWeights = new double[] { 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE };
+		gbl_contentPane.rowWeights = new double[] { 0.0, 0.0, 1.0, Double.MIN_VALUE };
+		contentPane.setLayout(gbl_contentPane);
+
+		JLabel lblNewLabel = new JLabel("IP");
+		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
+		gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel.anchor = GridBagConstraints.EAST;
+		gbc_lblNewLabel.gridx = 0;
+		gbc_lblNewLabel.gridy = 0;
+		contentPane.add(lblNewLabel, gbc_lblNewLabel);
+
+		comboIp = new JComboBox();
+		comboIp.setMinimumSize(new Dimension(200, 24));
+		comboIp.setPreferredSize(new Dimension(200, 24));
+		GridBagConstraints gbc_comboIp = new GridBagConstraints();
+		gbc_comboIp.insets = new Insets(0, 0, 5, 5);
+		gbc_comboIp.fill = GridBagConstraints.HORIZONTAL;
+		gbc_comboIp.gridx = 1;
+		gbc_comboIp.gridy = 0;
+		contentPane.add(comboIp, gbc_comboIp);
+
+		JLabel lblNewLabel_1 = new JLabel("Porta");
+		GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
+		gbc_lblNewLabel_1.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_1.anchor = GridBagConstraints.EAST;
+		gbc_lblNewLabel_1.gridx = 0;
+		gbc_lblNewLabel_1.gridy = 1;
+		contentPane.add(lblNewLabel_1, gbc_lblNewLabel_1);
+
+		txfPorta = new JTextField();
+		txfPorta.setText("1818");
+		txfPorta.setPreferredSize(new Dimension(200, 24));
+		txfPorta.setMinimumSize(new Dimension(200, 24));
+		GridBagConstraints gbc_txfPorta = new GridBagConstraints();
+		gbc_txfPorta.anchor = GridBagConstraints.WEST;
+		gbc_txfPorta.insets = new Insets(0, 0, 5, 5);
+		gbc_txfPorta.gridx = 1;
+		gbc_txfPorta.gridy = 1;
+		contentPane.add(txfPorta, gbc_txfPorta);
+
+		buttonIniciarServico = new JButton("Iniciar ServiÃ§o");
+		GridBagConstraints gbc_buttonIniciarServico = new GridBagConstraints();
+		gbc_buttonIniciarServico.anchor = GridBagConstraints.WEST;
+		gbc_buttonIniciarServico.insets = new Insets(0, 0, 5, 5);
+		gbc_buttonIniciarServico.gridx = 2;
+		gbc_buttonIniciarServico.gridy = 1;
+		contentPane.add(buttonIniciarServico, gbc_buttonIniciarServico);
+
+		buttonPararServico = new JButton("Parar ServiÃ§o");
+		GridBagConstraints gbc_buttonPararServico = new GridBagConstraints();
+		gbc_buttonPararServico.anchor = GridBagConstraints.WEST;
+		gbc_buttonPararServico.insets = new Insets(0, 0, 5, 0);
+		gbc_buttonPararServico.gridx = 3;
+		gbc_buttonPararServico.gridy = 1;
+		contentPane.add(buttonPararServico, gbc_buttonPararServico);
+
+		JScrollPane scrollPane = new JScrollPane();
+		GridBagConstraints gbc_scrollPane = new GridBagConstraints();
+		gbc_scrollPane.gridwidth = 4;
+		gbc_scrollPane.fill = GridBagConstraints.BOTH;
+		gbc_scrollPane.gridx = 0;
+		gbc_scrollPane.gridy = 2;
+		contentPane.add(scrollPane, gbc_scrollPane);
+
+		textArea = new JTextArea();
+		textArea.setEnabled(false);
+		textArea.setFont(new Font("Monospaced", Font.BOLD, 16));
+		textArea.setForeground(new Color(50, 205, 50));
+		textArea.setBackground(Color.BLACK);
+		scrollPane.setViewportView(textArea);
 	}
+
+	private Map<String, Client> mapaClientes = new HashMap<>();
+
+	/**
+	 * ReferÃªncia a esse prÃ³prio objeto depois de exportado.
+	 */
+	private Server server;
+
+	/**
+	 * Registo onde o objeto exportado serÃ¡ buscado pelo nome. Ã‰ o registro que
+	 * escuta na porta TCP/IP.
+	 */
+	private Registry registry;
+
+	/**
+	 * Chamado para fazer as configuraÃ§Ãµes de tela adicionais que nÃ£o sÃ£o feitas
+	 * no Window Builder.
+	 */
+	public void configurar() {
+
+		// Botao para parar inicia desabilitado.
+		buttonPararServico.setEnabled(false);
+
+		// Configura os ips no combobox.
+		List<String> lista = getIpsDisponiveis();
+		comboIp.setModel(new DefaultComboBoxModel<String>(new Vector<String>(lista)));
+		comboIp.setSelectedIndex(0);
+
+		// Configura o frame para encerrar a aplicaÃ§Ã£o ao clicar no botÃ£o fechar
+		// (X).
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		// Configura o frame para avisar os clientes que estÃ¡ sendo fechado.
+		this.addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent e) {
+				fecharTodosClientes();
+			}
+		});
+
+		// Configura o botao iniciar servico.
+		buttonIniciarServico.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				iniciarServico();
+			}
+		});
+
+		// Configura o botao parar servico.
+		buttonPararServico.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				pararServico();
+			}
+		});
+
+	}
+
+	protected void iniciarServico() {
+
+		String strPorta = txfPorta.getText().trim();
+
+		if (!strPorta.matches("[0-9]+") || strPorta.length() > 5) {
+			JOptionPane.showMessageDialog(this, "A porta deve ser um valor numÃ©rico de no mÃ¡ximo 5 dÃ­gitos!");
+			return;
+		}
+		int intPorta = Integer.parseInt(strPorta);
+		if (intPorta < 1024 || intPorta > 65535) {
+			JOptionPane.showMessageDialog(this, "A porta deve estar entre 1024 e 65535");
+			return;
+		}
+
+		try {
+
+			server = (Server) UnicastRemoteObject.exportObject(this, 0);
+			registry = LocateRegistry.createRegistry(intPorta);
+			registry.rebind(Server.NOME, server);
+
+			mostrar("ServiÃ§o iniciado.");
+
+			comboIp.setEnabled(false);
+			txfPorta.setEnabled(false);
+			buttonIniciarServico.setEnabled(false);
+
+			buttonPararServico.setEnabled(true);
+
+		} catch (RemoteException e) {
+			JOptionPane.showMessageDialog(this, "Erro criando registro, verifique se a porta jÃ¡ nÃ£o estÃ¡ sendo usada.");
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Avisa os clientes e encerra as atividades do servidor, mas nÃ£o fecha a
+	 * aplicaÃ§Ã£o.
+	 */
+	protected void pararServico() {
+		mostrar("SERVIDOR PARANDO O SERVIÃ‡O.");
+
+		fecharTodosClientes();
+
+		try {
+			UnicastRemoteObject.unexportObject(this, true);
+			UnicastRemoteObject.unexportObject(registry, true);
+
+			comboIp.setEnabled(true);
+			txfPorta.setEnabled(true);
+			buttonIniciarServico.setEnabled(true);
+
+			buttonPararServico.setEnabled(false);
+
+			mostrar("ServiÃ§o encerrado.");
+
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	/**
+	 * Desconecta todos os clientes.
+	 */
+	private void fecharTodosClientes() {
+		mostrar("DESCONECTANDO TODOS OS CLIENTES.");
+	}
+
+	/**
+	 * Busca no sistema todos os ips IPV4, filtrados por expressÃ£o regular.
+	 * 
+	 * @return
+	 */
+	private List<String> getIpsDisponiveis() {
+
+		List<String> addrList = new ArrayList<String>();
+		try {
+			Enumeration<NetworkInterface> ifaces = NetworkInterface.getNetworkInterfaces();
+
+			while (ifaces.hasMoreElements()) {
+				NetworkInterface ifc = ifaces.nextElement();
+				if (ifc.isUp()) {
+					Enumeration<InetAddress> addresses = ifc.getInetAddresses();
+					while (addresses.hasMoreElements()) {
+
+						InetAddress addr = addresses.nextElement();
+
+						String ip = addr.getHostAddress();
+
+						if (ip.matches("[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}")) {
+							addrList.add(ip);
+						}
+
+					}
+				}
+			}
+		} catch (SocketException e) {
+			e.printStackTrace();
+		}
+
+		return addrList;
+	}
+
+
+	private void mostrar(String string) {
+		textArea.append(" -> ");
+		textArea.append(string);
+		textArea.append("\n");
+	}
+
+	private void atualizarListasDeParticipantes() {
+		mostrar("Enviando lista atualizada de participantes para todos.");
+		try {
+			for (Client c : mapaClientes.values()) {
+				c.listaClients(new ArrayList<String>(mapaClientes.keySet()));
+			}
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
+	}
+
+
+	@Override
+	public void logoff(String nome) throws RemoteException {
+		mapaClientes.remove(nome);
+		mostrar(nome + " deixou a sala.");
+		atualizarListasDeParticipantes();
+	}
+
+	@Override
+	public void loginTorrent(String nome, Client cliente) throws RemoteException {
+		mostrar(nome + " se conectou.");
+
+		if (mapaClientes.get(nome) != null) {
+
+			mostrar("Retornando erro para tentativa de nome duplicado: " + nome);
+
+			throw new RemoteException("AlguÃ©m jÃ¡ estÃ¡ usando o nome: " + nome);
+		}
+
+		mapaClientes.put(nome, cliente);
+
+		mostrar(nome + " se conectou.");
+
+		atualizarListasDeParticipantes();
+		
+	}
+
+	@Override
+	public void download(String remetente, String destinatario, String mensagem) throws RemoteException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void compatilhar(String remetente, String mensagem) throws RemoteException {
+
+		for (Entry<String, Client> e : mapaClientes.entrySet()) {
+			if (e.getKey().equals(remetente)) {
+				continue;
+			} else {
+				try {
+				//	e.getValue().listaClients(remetente, mensagem);
+				} catch (Exception e1) {
+					e1.printStackTrace();
+				}
+			}
+		}
+
+		mostrar(remetente + " fala para todos: " + mensagem);
+	}
+
+
 
 }
