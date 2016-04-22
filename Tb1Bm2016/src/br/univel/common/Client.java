9f1e2292afccfd8e31a974fabfe0033e501ac129
Emerson Magalhaes <emdemagalhaes@gmail.com>
2016-04-22 16:14:11 -0300

    Criado os metodos listarClients e listarArquivos.

--- Tb1Bm2016/src/br/univel/common/Client.java
+++ Tb1Bm2016/src/br/univel/common/Client.java
@@ -1,5 +1,16 @@
 package br.univel.common;
 
+import java.rmi.RemoteException;
+import java.util.List;
+
 public interface Client {
+
+	//lista os clientes conectados
+	public void listaClients(List<String> lista) throws RemoteException;
+	
+	//recebe a lista de arquivos dos cliente
+	public void listaArquivos(String remetente, String mensagem) throws RemoteException;
+
+
 
 }
